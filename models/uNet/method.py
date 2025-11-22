8be06e5d32b0c8f5590347281af427619387b8f0
Maegon <manutonymanu@gmail.com>
2025-11-22 15:24:57 +0100

    Implementation of the custom 3D UNet

---
+++ models/uNet/method.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+def dice(pred, target, epsilon=1e-07):
+    """
+    DICE metric provides a measure of the similarity between the predicted segmentation
+    and the ground truth segmentation.
+    Params
+    -------
+
+    Returns
+    -------
+    """
+
+    pred_copy = pred.clone()
+
+    pred_copy[pred_copy < 0] = 0
+    pred_copy[pred_copy > 1] = 1
+
+    intersection = abs(torch.sum(pred_copy* target))
+    union = abs(torch.sum(pred_copy) + torch.sum(target))
+    coeff = (2 * intersection + epsilon) / (union + epsilon)
+
+    return coeff
+
+def train(model,
+         train_set: DataLoader,
+         criterion,
+         optimizer,
+         device,
+         epochs,
+         val_set: DataLoader=None):
+    """
+    Train a PyTorch model
+    """
+
+    model = model.to(device)
+
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        running_loss = 0
+        running_dc = 0
+
+        loop = tqdm(train_set, desc=f"Epoch {epoch+1}/{epochs}", leave=False)
+
+        for idx, img_mask in enumerate(loop):
+            img, mask = img_mask[0].float().to(device), img_mask[1].float.to(device)
+
+            pred = model(img)
+            optimizer.zero_grad()
+
+            dc = dice(pred, mask)
+            loss = criterion(pred, mask)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            running_dc += dc.item()
+
+
+        epoch_loss = running_loss / len(train_set.dataset)
+        epoch_dc = running_dc / len(train_set.dataset)
+        print(f"Epoch {epoch+1}/{epochs} - Train Loss: {epoch_loss:.4f}")
+
+        if val_set is not None:
+            val_loss = evaluate(model, val_set, criterion, device)
+            print(f"Val Loss:   {val_loss:.4f}")
+
+def evaluate(model, 
+             data_set: DataLoader,
+             criterion,
+             device):
+    """
+    Evaluate the model on a dataset.
+    """
+
+    model.eval()
+    total_loss = 0
+    # total_dc = 0
+
+    with torch.no_grad():
+        for inputs, labels in data_set:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            # dc = dice(outputs, mask)
+            total_loss += loss.item() * inputs.size(0)
+            # total_dc += dc.item()
+    
+    return total_loss/len(data_set.dataset)
---
+++ models/uNet/ublock3d.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+class DoubleConv3d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.operation = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        y = self.operation(x)
+        return y
+
+class Encoder3d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.conv = DoubleConv3d(in_channels, out_channels)
+        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        c = self.conv(x)
+        p = self.pool(c)
+        return c, p
+
+
+class Decoder3d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.deconv = nn.ConvTranspose3d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.conv = DoubleConv3d(in_channels, out_channels)
+
+    def forward(self, t1, t2):
+        t1 = self.deconv(t1)
+        
+        x = torch.cat([t1, t2], 1)
+        y = self.conv(x)
+        return y
