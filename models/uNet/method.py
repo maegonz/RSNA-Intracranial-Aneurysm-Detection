10a2f32b6f824b0cb943586e8c6b5808b19af1c9
Maegon <manutonymanu@gmail.com>
2025-11-23 10:37:41 +0100

    Edit train and evaluate function

--- models/uNet/method.py
+++ models/uNet/method.py
@@ -5,15 +5,21 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-def dice(pred, mask, epsilon=1e-07):
+def dice_loss(pred, mask, epsilon=1e-07):
     """
     DICE metric provides a measure of the similarity between the predicted segmentation
     and the ground truth segmentation.
+
     Params
     -------
+    pred
 
+    mask
+    
     Returns
     -------
+    dice: float
+        Float between [0;1]
     """
 
     pred_copy = pred.clone()
@@ -24,31 +30,32 @@ def dice(pred, mask, epsilon=1e-07):
     intersection = abs(torch.sum(pred_copy* mask))
     union = abs(torch.sum(pred_copy) + torch.sum(mask))
     coeff = (2 * intersection + epsilon) / (union + epsilon)
-    dice = 1 - coeff
 
-    return dice
+    return 1 - coeff
 
-# def train_step(model,
-#                volume,
-#                label,
-#                mask=None):
+def train_step(model,
+               volume,
+               label,
+               mask=None):
+    clf_logits, seg_pred = model(volume)
+    clf_loss = F.binary_cross_entropy_with_logits(clf_logits, label)
+
+    if mask is not None:
+        bce = F.binary_cross_entropy(seg_pred, mask)
+        dc = dice_loss(seg_pred, mask)
+        seg_loss = bce + dc
+    else:
+        seg_loss, dc = 0, 0
     
-#     clf_logits, seg_pred = model(volume)
-
-#     clf_loss = F.binary_cross_entropy_with_logits(clf_logits, label.float())
-
-#     if mask is not None:
-#         bce = F.binary_cross_entropy(seg_pred, mask)
-#         dice = dice(seg_pred, mask)
-#         seg_loss = bce + dice
+    total_loss = clf_loss + seg_loss
+    return total_loss, clf_loss, dc
 
 def train(model,
-         train_set: DataLoader,
-         criterion,
+         train_dl: DataLoader,
          optimizer,
          device,
          epochs,
-         val_set: DataLoader=None):
+         val_dl: DataLoader=None):
     """
     Train a PyTorch model
     """
@@ -60,17 +67,16 @@ def train(model,
         running_loss = 0
         running_dc = 0
 
-        loop = tqdm(train_set, desc=f"Epoch {epoch+1}/{epochs}", leave=False)
+        loop = tqdm(train_dl, desc=f"Epoch {epoch+1}/{epochs}", leave=False)
 
-        for idx, img_mask in enumerate(loop):
-            img, mask = img_mask[0].float().to(device), img_mask[1].float.to(device)
+        for item in loop:
+            volume = item['volume'].to(device)
+            label = item['label'].to(device)
+            mask = item['mask'].to(device)
 
-            pred = model(img)
+            loss, clf_loss, dc = train_step(model, volume, label, mask)
+
             optimizer.zero_grad()
-
-            dc = dice(pred, mask)
-            loss = criterion(pred, mask)
-
             loss.backward()
             optimizer.step()
 
@@ -78,17 +84,16 @@ def train(model,
             running_dc += dc.item()
 
 
-        epoch_loss = running_loss / len(train_set.dataset)
-        epoch_dc = running_dc / len(train_set.dataset)
-        print(f"Epoch {epoch+1}/{epochs} - Train Loss: {epoch_loss:.4f}")
+        epoch_loss = running_loss / len(train_dl.dataset)
+        epoch_dc = running_dc / len(train_dl.dataset)
+        print(f"Epoch {epoch+1}/{epochs} - Train Loss: {epoch_loss:.4f} - Train DC: {epoch_dc:.4f}")
 
-        if val_set is not None:
-            val_loss = evaluate(model, val_set, criterion, device)
+        if val_dl is not None:
+            val_loss = evaluate(model, val_dl, device)
             print(f"Val Loss:   {val_loss:.4f}")
 
 def evaluate(model, 
-             data_set: DataLoader,
-             criterion,
+             data_loader: DataLoader,
              device):
     """
     Evaluate the model on a dataset.
@@ -99,12 +104,15 @@ def evaluate(model,
     # total_dc = 0
 
     with torch.no_grad():
-        for inputs, labels in data_set:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+        for item in data_loader:
+            volume = item['volume'].to(device)
+            label = item['label'].to(device)
+            
+            clf_logits, seg_logits = model(volume)
+            
+            loss = F.binary_cross_entropy_with_logits(clf_logits, label)
+            total_loss += loss.item()
             # dc = dice(outputs, mask)
-            total_loss += loss.item() * inputs.size(0)
             # total_dc += dc.item()
     
-    return total_loss/len(data_set.dataset)
+    return total_loss/len(data_loader)
